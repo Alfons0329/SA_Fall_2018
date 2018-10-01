7ed5a6789ed7b34d288e76a3e76d09c8579baecf
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-01 22:58:42 +0800

    SAHW2 working on problem 1, almost done

--- HW2/test.sh
+++ HW2/test.sh
@@ -1,29 +1,6 @@
 #!/bin/sh
-#ls -ARlS | awk '{if(NF>=9) printf("%s \n", $0)}' | sort -nr -k 5 | awk -f akt.sh
-ls -ARlS | awk '{if(NF>=9) printf("%s \n", $0)}' | sort -nr -k 5 | awk 'BEGIN { \
-    total_dir=0 \
-    total_file=0 \
-    total_size=0 \
-} \
-{ \
-    if($0~/^d.*/){ \
-       #printf(" %s is %d th directory \n", $0, ++total_dir) \
-        ++total_dir \
-    } \
-    else if($0!/^d.*/){ \
-        total_size += $5 \
-        if(total_file < 5){ \
-           #printf("%s is %d th big file with size %d and total size %d \n", $0, ++total_file, $5, total_size) \
-           printf("%d:%d %s \n", ++total_file, $5, $9) \
-        } \
-    } \
-} \
-
-END { \
-    printf("Dir num: %d\n",total_dir) \
-    printf("File num: %d\n", total_file) \
-    printf("Total: %d", total_size) \
-}'
+#ls -ARlS | awk '{if(NF>=9) printf("%sn", $0)}' | sort -nr -k 5 | awk -f akt.sh
+ls -ARlS | awk '{if(NF>=9) printf("%sn", $0)}' | sort -nr -k 5 | awk 'BEGIN { total_dir=0 total_file=0 total_size=0 }{ if($0~/^d.*/){ #printf(" %s is %d th directoryn", $0, ++total_dir) ++total_dir} else if($0!/^d.*/){total_size += $5 if(total_file < 5){ #printf("%s is %d th big file with size %d and total size %dn", $0, ++total_file, $5, total_size) printf("%d:%d %sn", ++total_file, $5, $9)}}} END { printf("Dir num: %d\nFile num:%d\nTotal: %d ",total_dir, total_file, total_size)}'
 
 
 
