e77330ad6d3f7b7d4d4fc8498bda7151ca40f1b6
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-01 22:53:20 +0800

    SAHW2 working on problem 1, almost done

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ls -ARlS | awk '{if(NF>=9) printf("%s \n", $0)}' | sort -nr -k 5 | awk -f akt.sh
-
-#'BEGIN{ fn=0 }{if(NF >= 1){fn++; print $0, fn}} END{printf("%d and %d \n",fn ,NR)}'
-#
-
