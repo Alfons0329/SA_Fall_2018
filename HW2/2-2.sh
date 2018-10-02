8d0fff6787a30618bb7b608b8335648798dfafb6
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-03 00:27:12 +0800

    Working on SAHW2 Prob 2

--- HW2/2-2.sh
+++ HW2/2-2.sh
@@ -10,3 +10,17 @@ else
     name=**&m_teaname=**&m_cos_id=**&m_cos_code=**&m_crstime=**&m_crsoutline=**&m_costype=**' >> class.json
 fi
 
+#JSON parsing, parse cos_ename, cos_time(including location after - mark)
+#use this parsing function with awk and sed to output the value of certain field
+function get_json_value()
+{
+    local json_file=$1
+    local c_ename="cos_ename"
+    local c_time="cos_time"
+
+    #use the  , : } as filed separator
+    print json_file | awk ''
+    printf("course name %d, course time and location %d \n")
+}
+
+get_json_value class.json
