8ba6d817bb7e0c02087fed29180506a87bc9b909
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-05 01:10:06 +0800

    Working on SAHW2 Prob 2, Time parsing almost done

--- HW2/2-2.sh
+++ HW2/2-2.sh
@@ -2,9 +2,9 @@
 #unit test: can crawl the course table, 1002 ok
 if [ -e "class.json" ]; #check if the course exists
 then
-    echo "Course table exists \n"
+    echo "Course table exists "
 else
-    echo "Course table does not exist \n"
+    echo "Course table does not exist "
     curl 'https://timetable.nctu.edu.tw/?r=main/get_cos_list' --data \
     'm_acy=107&m_sem=1&m_degree=3&m_dep_id=17&m_group=**&m_grade=**&m_class=**&m_option=**&m_crs \
     name=**&m_teaname=**&m_cos_id=**&m_cos_code=**&m_crstime=**&m_crsoutline=**&m_costype=**' >> class.json
@@ -12,21 +12,14 @@ fi
 
 #JSON parsing, parse cos_ename, cos_time(including location after - mark)
 #use this parsing function with awk and sed to output the value of certain field
-function get_json_value()
-{
-    local json_file=$1
-    local c_ename="cos_ename"
-    local c_time="cos_time"
 
-    local parsed_first="parsed_json.txt"
-    local parsed_second="time_to_cos.txt"
+json_file="class.json"
+parsed_first="parsed_json.txt"
+parsed_second="time_to_cos.txt"
+data_base="db.txt"
 
-    #use the  , : } as file delimitor
-    cat $json_file | awk ' BEGIN {FS="[,;}{:]"} { for( nf_cnt=0; nf_cnt<NF; nf_cnt++ ){ if( $(nf_cnt)~/"cos_time"/) { printf("%s", $(nf_cnt+1)) } else if( $(nf_cnt)~/"cos_ename"/){ print ",", $(nf_cnt+1) } } }' > $parsed_first
-    #printf("course name %d, course time and location %d \n")
+#use the  , : } as file delimitor
+cat $json_file | awk ' BEGIN { FS="[,;}{:]" } { for( nf_cnt=0; nf_cnt<NF; nf_cnt++ ){ if( $(nf_cnt)~/"cos_time"/) { printf("%s", $(nf_cnt+1)) } else if( $(nf_cnt)~/"cos_ename"/){ print ",", $(nf_cnt+1) } } } ' > $parsed_first
+cat $parsed_first | sed  's/\"//g; s/\-/\,/g' > $parsed_second
+cat $parsed_second | awk 'BEGIN { FS="[,\n]"; RS="" } {  for( nf_cnt=0; nf_cnt<NF; nf_cnt++ ){ if($nf_cnt ~/^[1-9].*/){ print "time: " $nf_cnt } } }'
 
-    cat $parsed_first | sed 's/\"//g' > $parsed_second 
-    
-}
-
-get_json_value class.json
