458d88c2fe833c18a0e84d236303584322c90f7a
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-08 22:04:24 +0800

    Doing conflice checking

--- HW2/2-2.sh
+++ HW2/2-2.sh
@@ -63,13 +63,14 @@ generate_list() {
     sed -i.bak 's/_off_/ off /g' "menu_db.txt"
 
     sed -i.bak 's/-/ /g' "menu_db.txt"
-    menu_db=$(cat "menu_db.txt")
 }
 
 
 
 
 write_db() {
+    cat "menu_db.txt" | less
+    menu_db=$(cat "menu_db.txt")
     sel=$(dialog --stdout --buildlist "Choose one" 200 200 200 $menu_db)
     #extracted the course name from the cos_name.txt with the selected number
     rm -f "cur_selected.txt"
@@ -81,7 +82,7 @@ write_db() {
 
         sel_name=$(cat "cos_data.txt" | awk -v sel_row="$i" '  BEGIN { i=0; FS="," } { ++i; if(i==sel_row){ printf("%s", $NF) } } ')
 
-        echo "$sel_name,$sel_time" >> "cur_selected.txt"
+        #echo "$sel_name,$sel_time" >> "cur_selected.txt"
         #change the menu_db from off to on
         sed -E -i.bak "s/off/on/$i" "menu_db.txt"
 
@@ -96,19 +97,19 @@ write_db() {
 
     #check the class conflict for two types
     #check the conflict b/w the selected class
-    cat "cur_selected.txt" | less
+    #cat "cur_selected.txt" | less
     #check the conflict b/w selected class and the class already on the time table
 
 }
 
 #-----------------------------------------------work flow-------------------------------------------------------------#
-for i in 1 2
+for i in 1 2 3
 do
     if [ -e "class.json" ];
     then
+    else
         generate_list
     fi
-    sel=$(dialog --stdout --buildlist "Choose one" 200 200 200 "menu_db.txt")
     #if [ $sel -eq 1 ];
     #then
     #    break
