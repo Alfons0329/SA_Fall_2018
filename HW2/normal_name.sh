ca1f965c8cee707e0f34eb79d957f153aa9d753f
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-10-10 21:39:26 +0800

    Now testing drawing table

--- HW2/normal_name.sh
+++ HW2/normal_name.sh
@@ -1,34 +1,49 @@
 #!/bin/sh
 rm -f "show1.txt"
 
-none='x'
-point='.'
-boundary='|'
-blank_short='               '
-blank_long='                  '
-time_split='= '
-long_split='=================== '
+none="x"
+point="."
+boundary="|"
+blank_short="               "
+blank_long="                  "
+time_split="= "
+long_split="=================== "
 
-monday='Mon'
-tuesday='Tue'
-wednesday='Wed'
-thursday='Thu'
-friday='Fri'
+monday="Mon"
+tuesday="Tue"
+wednesday="Wed"
+thursday="Thu"
+friday="Fri"
 
-name1
-name2
-name3
-name4
+name1=""
+name2=""
+name3=""
+name4=""
 
 #Use sed to make 13 character a line and next line, split with, 
+gen_table() {
+    rm -f $table
+    for i in 1 2 3 4 5 
+    do
+        for j in  "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L"
+        do
+            echo $i$j"," >> "empty_time.txt"
+        done
+    done
+}
+
 parse_name() {
     cp "selected_time.txt" "show_name.txt"
+    #remove the time to merge later
+    sed -E -i.bak 's/[1-9][A-Z],//g' "show_name.txt"
+    #split the string every 13 character long
     sed -E -i.bak 's/(.{13})/\1\,/g' "show_name.txt" 
+    cat "show_name.txt" | awk ''
 }   
 
-find_assign() {
-    
-}
+# find_assign() {
+
+# }
 
 
 print_firstline() {  
@@ -52,5 +67,4 @@ print_splitline() {
     printf "$allsplit\n" >> "show1.txt"
 }
 
-print_firstline
-print_splitline
+parse_name
