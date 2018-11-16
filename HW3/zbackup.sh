55dde36bfaf1813ad60991da2ceeae5f38965a7d
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-17 00:51:37 +0800

    Working on create the snapshot

--- HW3/zbackup.sh
+++ HW3/zbackup.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
+#------------------global variable declaration starts here---------------#
 data=""
 id=""
 filename=""
 rotate_cnt=""
 arg_cnt=0
+#------------------global variable declaration ends here---------------#
+
+#function to check if the id is legal
 check_id()
 {
-	case $id in
-
+	case $1 in
 		[0-9]*) #regex repeat
 			;;
 
@@ -16,13 +19,48 @@ check_id()
 			;;
 
 		*)
-			error "Invalid ID number!!"
+			error "Illegal ID number!!"
 			;;
 	esac
 }
+
+check_dataset()
+{
+	for data in $(zfs list | cut -d ' ' -f 1); 
+	do
+		if [ $1 = $data ] ; 
+		then
+			return
+		fi
+	done
+	error "Dataset does not exist!!"
+}
+
+
 create()
 {
+	#declare some required vars
+	local rot_cnt=$2 #rotation count
+	dataset=$1 #what the name for back up
 
+	#check if the id is illegal
+	check_id $1
+	
+	#adjust the rotate count
+	#if no specified rotate count, just set it as 20
+	if [ -z "$rot_cnt" ] ; 
+	then 
+		rotate_cnt=20; 
+	fi
+	
+	#illegal rotate count
+	if [ $rot_cnt -eq 0 ] ; 
+	then 
+		error "rotate count should in range [1, 20]"
+		exit 0; 
+	fi
+
+	#snapshot the data
 }
 
 list()
@@ -42,7 +80,6 @@ check_arg()
 		echo " Usage: zbackup [[--list | --delete | --export] target-dataset [ID] | [--import] target-dataset filename | target dataset [rotation count]] "
 		exit 0
 	fi
-
 }
 
 error()
@@ -80,9 +117,8 @@ main()
 			;;
 
 		*)
-			dataset=$1
-
-			create $2
+			#default, create the dataset $1 is the name and $2 is the max rotate count of that data set
+			create $1 $2
 			;;
 	esac
 
