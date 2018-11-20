fddc8d7081d6b3f7b0996a2dd5e9547c14b82eab
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-11-20 20:38:42 +0800

    Testcases for SAHW3 Problem 1

---
+++ HW3/prob1_testcase/set_perm.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+sudo chmod 751 hidden/
+sudo chmod 755 public/
+sudo chmod 773 upload/
+sudo chmod 755 hidden/treasure
+touch public/anony_can_dl_this_public
+touch hidden/treasure/anony_can_see_this_treasure
+touch hidden/anony_cant_see_this_hidden
